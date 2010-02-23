e23c80126065fa2f2b3d2973ac1f25eff4be2db0
verhelstbert <verhelstbert@f78174ba-1593-11df-8e3c-378b6dd812f8>
2010-02-23 20:47:16 +0000

    xml file works

---
+++ trunk/KohtopaDesktop/src/gui/MyHashMapEntryType.java
@@ -0,0 +1,23 @@
+package gui;
+
+import java.util.Map;
+import javax.xml.bind.annotation.XmlAttribute;
+
+public class MyHashMapEntryType {
+    @XmlAttribute // @XmlElement and @XmlValue are also fine
+    public String key;
+
+    @XmlAttribute // @XmlElement and @XmlValue are also fine
+    public String value;
+
+    public MyHashMapEntryType() {}
+    public MyHashMapEntryType(Map.Entry<String,String> e) {
+       key = e.getKey();
+       value = e.getValue();
+    }
+}
+
+
+
+
+
