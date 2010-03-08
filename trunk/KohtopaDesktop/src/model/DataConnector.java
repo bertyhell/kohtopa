87586e6d09343b0fd37926c75332df77c9256c01
jannesplyson <jannesplyson@f78174ba-1593-11df-8e3c-378b6dd812f8>
2010-03-08 17:07:55 +0000

    Het ophalen van de pictures is nu ook mogelijk alles gaat via het BufferedImage object

--- trunk/KohtopaDesktop/src/model/DataConnector.java
+++ trunk/KohtopaDesktop/src/model/DataConnector.java
@@ -1,9 +1,8 @@
 package model;
 
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -11,6 +10,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Collection;
 import javax.imageio.ImageIO;
 
 public class DataConnector {
@@ -33,6 +33,24 @@ public class DataConnector {
 	private static Connection geefVerbinding() throws SQLException {
 		return DriverManager.getConnection(DataBaseConstants.connectiestring, DataBaseConstants.un, DataBaseConstants.pw);
 	}
+
+        private static int getNextId(String tableName, String columnName){
+            try{
+               Connection conn = geefVerbinding();
+               try{
+                   Statement st = conn.createStatement();
+                   String query = DataBaseConstants.selectNextId.replaceAll("tableName", tableName).replaceAll("columnName", columnName);
+                   ResultSet rs = st.executeQuery(query);
+                   rs.next();
+                   return rs.getInt(1);
+               }finally{
+                   conn.close();
+               }
+            }catch(Exception exc){
+               System.out.println("getNextId: problems with connection: " + exc);
+            }
+            return 1;
+        }
 
 	public static void selectBuildingPreviews(ArrayList<Building> buildings) throws SQLException {
 		Connection conn = geefVerbinding();
@@ -61,19 +79,17 @@ public class DataConnector {
                    try{
                        ImageIO.write(bufferedImage, "jpg", baos);
                    }catch(Exception exc){
-                       System.out.println("error encoding buffered image");
+                       System.out.println("error in addPicture: encoding bufferedImage failed; " + exc);
                    }
-                   //BLOB blob = BLOB.createTemporary(conn, true, BLOB.DURATION_SESSION);
-                   //OutputStream os = blob.setBinaryStream(0);
                    PreparedStatement ps = conn.prepareStatement("insert into pictures values(?,?,?)");
                    try{
-                        int pictureId = getNextId("pictures", "pictureId");                        
+                        int pictureId = getNextId(DataBaseConstants.tablePictures, DataBaseConstants.pictureId);
                         ps.setInt(1, pictureId);
                         ps.setInt(2, rentableId);
                         ps.setBytes(3, baos.toByteArray());
                         ps.executeUpdate();                        
                    }catch(SQLException exc){
-                       System.out.println("error inserting picture: " + exc);
+                       System.out.println("error in addPicture: error inserting picture in database: " + exc);
                    }finally{
                        ps.close();
                    }
@@ -81,25 +97,53 @@ public class DataConnector {
                    conn.close();
                }
            }catch(Exception exc){
-               System.out.println("problems with connection");
+               System.out.println("error in addPicture: problems with connection : " + exc);
            }
         }
 
-        private static int getNextId(String tableName, String columnName){
+        public static ArrayList<Integer> getPictureIds(int rentableId){
+            ArrayList<Integer> ids = new ArrayList<Integer>();
             try{
-               Connection conn = geefVerbinding();
-               try{
-                   Statement st = conn.createStatement();
-                   String query = DataBaseConstants.selectNextId.replaceAll("tableName", tableName).replaceAll("columnName", columnName);                   
-                   ResultSet rs = st.executeQuery(query);
-                   rs.next();
-                   return rs.getInt(1);
-               }finally{
-                   conn.close();
-               }
+                Connection conn = geefVerbinding();
+                try{
+                    PreparedStatement ps = conn.prepareStatement(DataBaseConstants.selectPictureIds);
+                    ps.setInt(1, rentableId);
+                    ResultSet rs = ps.executeQuery();
+                    while(rs.next()){
+                        ids.add(rs.getInt(1));
+                    }
+                }finally{
+                    conn.close();
+                }
             }catch(Exception exc){
-               System.out.println("getNextId: problems with connection: " + exc);
+                System.out.println("error in addPicture: problems with connection : " + exc);
             }
-            return 1;
+            return ids;
+        }
+
+        public static BufferedImage getPicture(int pictureId){
+            BufferedImage bi = null;
+            try{
+                Connection conn = geefVerbinding();
+                try{
+                    ByteArrayInputStream bais = null;
+                    PreparedStatement ps = conn.prepareStatement(DataBaseConstants.selectPictureData);
+                    ps.setInt(1, pictureId);
+                    ResultSet rs = ps.executeQuery();
+                    while(rs.next()){                        
+                        bais = new ByteArrayInputStream(rs.getBytes(1));
+                    }
+                    if(bais != null){
+                        bi = ImageIO.read(bais);
+                    }else{
+                        System.out.println("error in getPicture: empty resultset");
+                    }
+                }finally{
+                    conn.close();
+                }
+            }catch(Exception exc){
+                System.out.println("error in getPicture: problems with connection: " + exc);
+            }
+            return bi;
         }
 }
--- trunk/KohtopaDesktop/test/testDataConnector.java
+++ trunk/KohtopaDesktop/test/testDataConnector.java
@@ -1,6 +1,7 @@
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.util.ArrayList;
 import javax.imageio.ImageIO;
 import model.DataConnector;
 
@@ -17,10 +18,15 @@ public class testDataConnector {
 
     public static void main(String[] args) {
         try{
-            BufferedImage bi = ImageIO.read(new File("menu.jpg"));
-            DataConnector.addPicture(1, bi);
+            //BufferedImage bi = ImageIO.read(new File("menu.jpg"));
+            //DataConnector.addPicture(1, bi);
+            ArrayList<Integer> ids = DataConnector.getPictureIds(1);
+            int id = ids.get(0);
+            BufferedImage bi = DataConnector.getPicture(id);            
+            ImageIO.write(bi, "jpg", new File("testMenu.jpg"));
         }catch(Exception exc){
             System.out.println(exc.getMessage());
         }
+
     }
 }
