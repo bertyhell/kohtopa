c89966da50529d39c3af24112882651e447437c1
verhelstbert <verhelstbert@f78174ba-1593-11df-8e3c-378b6dd812f8>
2010-03-09 15:20:54 +0000

    adding oracle connection, message tab

--- trunk/KohtopaDesktop/src/model/DataBaseConstants.java
+++ trunk/KohtopaDesktop/src/model/DataBaseConstants.java
@@ -12,8 +12,7 @@ public class DataBaseConstants {
 	public static String tableContracts = "contracts";
 	public static String tableConsumption = "consumption";
 	public static String tableMessages = "messages";
-        public static String tablePictures = "pictures";
-
+	public static String tablePictures = "pictures";
 	//addresses column labels
 	public static String addressId = "addressid";
 	public static String streetNumber = "street_number";
@@ -58,32 +57,34 @@ public class DataBaseConstants {
 	public static String dataConsumption = "data_consumption";
 	//messages column labels
 	public static String senderId = "senderid";
-	public static String recipientic = "recipientic";
+	public static String recipientId = "recipientid";
 	public static String dataSent = "date_sent";
 	public static String text = "text";
+	public static String rentableId = "rentableId";
+	public static String pictureData = "data";
+	//contracts column labels
+	public static String ownerId = "ownerid";
+	public static String renterId = "renterid";
+	public static String contractStart = "contract_start";
+	public static String contractEnd = "contract_end";
+	//pictures column labels
+	public static String pictureId = "pictureid";
+	public static String data = "data";
 
-        public static String pictureId = "pictureId";
-        public static String rentableId = "rentableId";
-        public static String pictureData = "data";
+
+
 
 	//dataconnector connection strings
-        /*
-        public static String un = "kohtopa";
-        public static String pw = "e=mc**2";
-        public static String driver = "com.mysql.jdbc.Driver";
-        public static String connectiestring = "jdbc:mysql://localhost:3306/kohtopa";
-        */
-        
-        //driver and connectionstring for oracle express
+	//driver and connectionstring for oracle express
+	public static String un = "system";
+	public static String pw = "admin";
+	public static String driver = "oracle.jdbc.OracleDriver";
 
-        public static String un = "system";
-        public static String pw = "admin";
-        public static String driver = "oracle.jdbc.OracleDriver";
-        public static String connectiestring = "jdbc:oracle:thin:@localhost:1521:XE";
-	
+	//public static String connectiestring = "jdbc:oracle:thin:@localhost:1521:XE";
+	public static String connectiestring = "jdbc:oracle:thin:@192.168.58.128:1521:XE";
 	//dataconnector statement strings
-    public static String selectBuildingPreviews = "SELECT " + buildingId + "," + streetNumber + "," + street + "," + zipCode + "," + city + " FROM " + tableBuildings + " b join " + tableAddressess + " a on a." + addressId + " = b." + addressId;    
-    public static String selectNextId = "with x as ( select columnName, rank() over(order by columnName)-2147483649 as rn from tableName), y as (select columnName, case  when rn <> columnName then rn else null end as rn from x) select case when min(rn) is null then case when count(1) is null then -2147483648 else count(1) - 2147483648 end else min(rn) end as id from y";
-    public static String selectPictureIds = ("select " + pictureId + " from " + tablePictures + " where " + rentableId + " = ?");
-    public static String selectPictureData = ("select " + pictureData + " from " + tablePictures + " where " + pictureId + " = ?");
+	public static String selectBuildingPreviews = "SELECT " + buildingId + "," + streetNumber + "," + street + "," + zipCode + "," + city + " FROM " + tableBuildings + " b join " + tableAddressess + " a on a." + addressId + " = b." + addressId;
+	public static String selectNextId = "with x as ( select columnName, rank() over(order by columnName)-2147483649 as rn from tableName), y as (select columnName, case  when rn <> columnName then rn else null end as rn from x) select case when min(rn) is null then case when count(1) is null then -2147483648 else count(1) - 2147483648 end else min(rn) end as id from y";
+	public static String selectPictureIds = ("select " + pictureId + " from " + tablePictures + " where " + rentableId + " = ?");
+	public static String selectPictureData = ("select " + pictureData + " from " + tablePictures + " where " + pictureId + " = ?");
 }
