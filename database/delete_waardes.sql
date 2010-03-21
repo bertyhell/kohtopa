69eb4fa9f5e82b4429e26af5c205cd4a43ad68f0
jellevdd <jellevdd@f78174ba-1593-11df-8e3c-378b6dd812f8>
2010-03-21 13:06:04 +0000

    database/perl poging 2 :)

---
+++ database/delete_waardes.sql
@@ -0,0 +1,12 @@
+DELETE FROM buildings;
+DELETE FROM rentables;
+DELETE FROM persons;
+DELETE FROM furniture;
+DELETE FROM addresses;
+DELETE FROM contract;
+DELETE FROM consumption;
+DELETE FROM messages;
+DELETE FROM pictures;
+DELETE FROM constants;
+DELETE FROM Invoice;
+DELETE FROM Tasks;
