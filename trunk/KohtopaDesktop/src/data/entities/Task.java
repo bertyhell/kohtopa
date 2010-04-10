35e33b22867b65a3a759c637279da814ad03c797
jellevdd <jellevdd@f78174ba-1593-11df-8e3c-378b6dd812f8>
2010-04-10 17:21:46 +0000

    Kalender partim 2: taken deels geimplementeerd, alle taken zitten in model, deze zorgt ervoor dat ze correct weergegeven zijn. Op een datum klikken geeft de taken van die dag weer. Deze kunnen zo makkelijk aangepast worden. De datums zitten in een lijst, gesorteerd op datum en dan alfabetisch. De icoontjes die nu boven kalender staan zijn atm nutteloos, maar het is eventueel mogelijk om die te gebruiken voor als men datum kent of eventueel terugkerende taken wil ingeven. Muisscrolls voor maanden geimplementeerd

---
+++ trunk/KohtopaDesktop/src/data/entities/Task.java
@@ -0,0 +1,56 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package data.entities;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author Jelle
+ */
+public class Task implements Comparable<Task> {
+    private String description;
+    private Date date;
+
+    public Task(String description, Date date) {
+        this.description = description;
+        this.date = date;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return DateFormat.getTimeInstance(DateFormat.SHORT).format(date)+": "+description;
+    }
+
+
+    public int compareTo(Task o) {
+        if(this.getDate().before(o.getDate())) {
+            return -1;
+        } else if(o.getDate() == this.getDate()) {
+            return this.getDescription().compareTo(o.getDescription());
+        } else {
+            return 1;
+        }
+    }
+
+}
--- trunk/KohtopaDesktop/src/gui/calendartab/CalendarModel.java
+++ trunk/KohtopaDesktop/src/gui/calendartab/CalendarModel.java
@@ -5,23 +5,101 @@
 
 package gui.calendartab;
 
+import data.entities.Task;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerDateModel;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 /**
- *
+ * Represents a model for a CalendarPanel, it listens to the spinner,
+ * functions as a Calendar and keeps track of tasks, tasks are put in a HashMap
+ * with as key time(ms)/86400000(this represents days since beginning of calendar),
+ * this cannot be a date because  a date has hours too.
  * @author Jelle
  */
 public class CalendarModel extends GregorianCalendar implements ChangeListener {
+    public static final long CONVERTMSTODAY = 86400000;
     CalendarPanel pane;
-    
-    protected CalendarModel(int year, int month, CalendarPanel pane) {
+    HashMap<Long,ArrayList<Task>> tasks;
+
+    protected CalendarModel(int year, int month, CalendarPanel panel) {
         super(year, month, 1);
-        this.pane = pane;
+        this.pane = panel;
+        tasks = new HashMap<Long,ArrayList<Task>>();
+
+        // testcase, should later get data from database(when there are tasks
+        // in it :))
+        // in the database implementation they should be in order
+        this.add(Calendar.HOUR, 5);
+        addTask(new Task("test3", this.getTime()));
+        this.add(Calendar.HOUR, -1);
+        addTask(new Task("test20342°4831840918240980409217804971409704720", this.getTime()));
+        this.add(Calendar.HOUR, -1);
+        addTask(new Task("test1", this.getTime()));
+        this.add(Calendar.HOUR, 4);
+        addTask(new Task("test4", this.getTime()));
+        this.add(Calendar.DATE, 2);
+        addTask(new Task("nieuwe dag", this.getTime()));
+        this.set(Calendar.HOUR, 0);
+        this.add(Calendar.DATE, -2);
+
+
+
+
+    }
+
+    public void addTask(Task task) {
+        long key = task.getDate().getTime()/CONVERTMSTODAY;
+        if(!tasks.containsKey(key)) {
+            System.out.println("new date found");
+            tasks.put(key,new ArrayList<Task>());
+        }
+        
+        tasks.get(key).add(task);
+    }
+
+    public void removeTask( Task task) {
+        long key = task.getDate().getTime()/CONVERTMSTODAY;
+        if(tasks.containsKey(key))
+            tasks.get(key).remove(task);
+    }
+
+    public void updateTask(Date date, Task task, Task newTask) {
+        long key = task.getDate().getTime()/CONVERTMSTODAY;
+        if(tasks.containsKey(key)) {
+            tasks.get(key).remove(task);
+            tasks.get(key).add(newTask);
+        }
+    }
+
+    public boolean hasTasksForDay(Date day) {
+        long key = day.getTime()/CONVERTMSTODAY;
+        return tasks.containsKey(key);
+
+    }
+
+    public HashMap<Long,ArrayList<Task>> getTasks() {
+        return tasks;
+    }
+
+    /**
+     * Getter for tasks for a day
+     * @param day the day, hours, minutes, seconds and miliseconds do not matter
+     * @return arrayList containing tasks for that day, this will be an empty
+     * array if there are no tasks
+     */
+    public ArrayList<Task> getTasksForDay(Date day) {
+        long key = day.getTime()/CONVERTMSTODAY;
+        if(tasks.containsKey(key))
+            return tasks.get(key);
+        return new ArrayList<Task>();
     }
 
     public void setDate(int year, int month) {
