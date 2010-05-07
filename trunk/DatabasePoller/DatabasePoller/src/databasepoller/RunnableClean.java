dc3924bf6f545387336f70238c2c19bb478f92ab
verhelstbert <verhelstbert@f78174ba-1593-11df-8e3c-378b6dd812f8>
2010-05-07 11:44:28 +0000

    polling program to check for invoices to be send

---
+++ trunk/DatabasePoller/DatabasePoller/src/databasepoller/RunnableClean.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package databasepoller;
+
+/**
+ *
+ * @author Bert Verhelst <verhelst_bert@hotmail.com>
+ */
+class RunnableClean implements Runnable{
+
+	public void run() {
+	}
+
+}
