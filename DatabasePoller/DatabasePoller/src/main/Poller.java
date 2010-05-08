32b5f5c56a91beb2ea25d3850be7ce9df2366694
verhelstbert <verhelstbert@f78174ba-1593-11df-8e3c-378b6dd812f8>
2010-05-08 08:50:54 +0000

    tasks in different package

---
+++ DatabasePoller/DatabasePoller/src/main/Poller.java
@@ -0,0 +1,27 @@
+package main;
+
+import tasks.RunnableTask;
+import tasks.RunnableInvoice;
+import tasks.RunnableClean;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ * @author Bert Verhelst
+ */
+public class Poller {
+
+	public static void main(String args[]) {
+		DataConnector.init();
+
+		ScheduledExecutorService deamonInvoices = Executors.newSingleThreadScheduledExecutor();
+		ScheduledExecutorService deamonTask = Executors.newSingleThreadScheduledExecutor();
+		ScheduledExecutorService deamonClean = Executors.newSingleThreadScheduledExecutor();
+
+		deamonInvoices.scheduleAtFixedRate(new RunnableInvoice(), 0, 20, TimeUnit.SECONDS);
+		deamonTask.scheduleAtFixedRate(new RunnableTask(), 0, 5, TimeUnit.SECONDS);
+		deamonClean.scheduleAtFixedRate(new RunnableClean(), 0, 24, TimeUnit.HOURS);
+	}
+}
---
+++ DatabasePoller/DatabasePoller/src/tasks/RunnableClean.java
@@ -0,0 +1,13 @@
+package tasks;
+
+/**
+ *
+ * @author Bert Verhelst <verhelst_bert@hotmail.com>
+ */
+public class RunnableClean implements Runnable{
+
+	public void run() {
+		System.out.println("cleaning database");
+	}
+
+}
